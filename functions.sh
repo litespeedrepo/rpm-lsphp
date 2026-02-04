bd1b6b88a58f2ac39ccd178eaf92ed7f32794ff8
Code-Egg <eric780217@gmail.com>
2026-02-04 13:19:33 +0800

    modified:   functions.sh

--- functions.sh
+++ functions.sh
@@ -308,5 +308,5 @@ upload_to_server(){
 
 gen_dev_release(){
     echoG '- Generate dev release'
-    ssh -oStrictHostKeyChecking=no root@${target_server} -t "/var/www/gen_rpm_release.sh ${EPEL_TAG} ${TARGET_FD} \"$new_rpms\""
+    ssh -oStrictHostKeyChecking=no -T root@${target_server} "/var/www/gen_rpm_release.sh ${EPEL_TAG} ${TARGET_FD} \"$new_rpms\""
 }
